185c1a43905c019185a940acec70ea6ecbbc97d2
Patrik Michl <xmichl@mendelu.cz>
2025-09-05 11:45:11 +0200

    cleanup

--- api/src/schemas/badge.py
+++
@@ -1,6 +0,0 @@
-badge_tag = {
-    "name": "badge",
-    "description": "Endpoints for your badges.",
-}
-
-badge_collection_name = "badges"
--- logger/src/main.py
+++
@@ -1,6 +0,0 @@
-from RabbitMq import RabbitMq
-from Database import Database
-
-if __name__ == "__main__":
-    # Database.init_db()
-    RabbitMq().start_consuming()
